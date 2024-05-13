c2d4d4c0cae6ab2cc49ad6319ff36a9e16df8e1f
zainab-kassim <kassimzainab11@gmail.com>
2024-05-13 15:16:53 +0100

    update5

--- src/components/career/career.jsx
+++ src/components/career/career.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import '../../styles/career.css';
-import graph1 from '../image/Mask group (1).png';
-import img1 from '../image/Einstein_patentoffice.jpg';
-import img2 from '../image/Einstein-with-habicht-and-solovine.jpg';
-import img3 from '../image/the newyorktimes.png';
-import img4 from '../image/Albert_Einstein_(Nobel).png';
-import img5 from '../image/lossy-page1-440px-League_of_Nations_Commission_067.tif.jpg';
-import img6 from '../image/1919_eclipse_positive.jpg';
-import img7 from '../image/MillikanLemaitreEinstein.jpg';
-import img8 from '../image/Albert_Einstein_photo_1920.jpg';
-import img9 from '../image/440px-Photoelectric_effect_in_a_solid_-_diagram.svg.png';
-import img10 from '../image/Albert_Einstein_1921_(re-cropped).jpg';
-import img11 from '../image/anothernews.jpg';
-import img12 from '../image/Niels_Bohr_Albert_Einstein4_by_Ehrenfest_cr.jpg';
-import img13 from '../image/Solvay_conference_1927.jpg';
+import career_graph1 from '../image/Mask group (1).png';
+import career_img1 from '../image/Einstein_patentoffice.jpg';
+import career_img2 from '../image/Einstein-with-habicht-and-solovine.jpg';
+import career_img3 from '../image/the newyorktimes.png';
+import career_img4 from '../image/Albert_Einstein_(Nobel).png';
+import career_img5 from '../image/lossy-page1-440px-League_of_Nations_Commission_067.tif.jpg';
+import career_img6 from '../image/1919_eclipse_positive.jpg';
+import career_img7 from '../image/MillikanLemaitreEinstein.jpg';
+import career_img8 from '../image/Albert_Einstein_photo_1920.jpg';
+import career_img9 from '../image/440px-Photoelectric_effect_in_a_solid_-_diagram.svg.png';
+import career_img10 from '../image/Albert_Einstein_1921_(re-cropped).jpg';
+import career_img11 from '../image/anothernews.jpg';
+import career_img12 from '../image/Niels_Bohr_Albert_Einstein4_by_Ehrenfest_cr.jpg';
+import careet_img13 from '../image/Solvay_conference_1927.jpg';
 
 
 
@@ -21,15 +21,15 @@ function career() {
     return (
         <div>
             <div className="container">
-                <hr />
-                <h1 className="Header1">Early academic career (1908-1933): </h1>
+                <hr className="career-hr" />
+                <h1 className="career-Header">Early academic career (1908-1933): </h1>
                 <div className="clearfix">
-                    <img src={img1} id="image" className=" col-xl-6  col-md-6  col-sm-12`   float-md-start mb-3 me-md-3 " alt="" />
-                    <section className="text">
+                    <img src={career_img} id="career-image" className=" col-xl-6  col-md-6  col-sm-12`   float-md-start mb-3 me-md-3 " alt="" />
+                    <section className="career-text">
                         Einstein's career sent him to multiple countries.He earned his doctorate from the University of Zurich in 1905 and subsequently took on professor positions in Zurich (1909), Prague (1911) and Zurich again (1912).
                         Einstein's sabbatical as a civil servant approached its end in 1908, when he secured a junior teaching position at the University of Bern.
                     </section>
-                    <section className="text">
+                    <section className="career-text">
                         <p />In 1909, a lecture on relativistic electrodynamics that he gave at the University of Zurich, much admired by Alfred Kleiner, led to Zürich's luring him away from Bern with a newly created associate professorship.Promotion to a full professorship followed in April 1911, when he accepted a chair at the German Charles-Ferdinand University in Prague, a move which required him to become an Austrian citizen of the Austro-Hungarian Empire. His time in Prague saw him producing eleven research papers.Einstein's sabbatical as a civil servant approached its end in 1908, when he secured a junior teaching position at the University of Bern.
                         In 1909, a lecture on relativistic electrodynamics that he gave at the University of Zurich, much admired by Alfred Kleiner, led to Zürich's luring him away from Bern with a newly created associate professorship. Promotion to a full professorship followed in April 1911, when he accepted a chair at the German Charles-Ferdinand University in Prague, a move which required him to become an Austrian citizen of the Austro-Hungarian Empire.
                         His time in Prague saw him producing eleven research papers.
@@ -42,10 +42,10 @@ function career() {
 
                 <section>
                     <div className="clearfix">
-                        <img src={img2} id="image2" className="col-md-5 float-md-end mb-2 ms-md-3" alt="image" />
-                        <div className="text"> In July 1912, he returned to his alma mater, the ETH Zurich, to take up a chair in theoretical physics. His teaching activities there centered on thermodynamics and analytical mechanics, and his research interests included the molecular theory of heat, continuum mechanics and the development of a relativistic theory of gravitation. In his work on the latter topic, he was assisted by his friend, Marcel Grossmann, whose knowledge of the kind of mathematics required was greater than his own.
+                        <img src={career_img2} id="career-image2" className="col-md-5 float-md-end mb-2 ms-md-3" alt="image" />
+                        <div className="career-text"> In July 1912, he returned to his alma mater, the ETH Zurich, to take up a chair in theoretical physics. His teaching activities there centered on thermodynamics and analytical mechanics, and his research interests included the molecular theory of heat, continuum mechanics and the development of a relativistic theory of gravitation. In his work on the latter topic, he was assisted by his friend, Marcel Grossmann, whose knowledge of the kind of mathematics required was greater than his own.
                         </div>
-                        <div className="text">
+                        <div className="career-text">
                             In the spring of 1913, two German visitors, Max Planck and Walther Nernst, called upon Einstein in Zürich in the hope of persuading him to relocate to Berlin. They offered him membership of the Prussian Academy of Sciences, the directorship of the planned Kaiser Wilhelm Institute for Physics and a chair at the Humboldt University of Berlin that would allow him to pursue his research supported by a professorial salary but with no teaching duties to burden him. Their invitation was all the more appealing to him because Berlin happened to be the home of his latest girlfriend, Elsa Löwenthal. He duly joined the Academy on 24 July 1913, and moved into an apartment in the Berlin district of Dahlem on 1 April 1914. He was installed in his Humboldt University position shortly thereafter.The outbreak of the First World War in July 1914 marked the beginning of Einstein's gradual estrangement from the nation of his birth. When the "Manifesto of the Ninety-Three" was published in October 1914—a document signed by a host of prominent German thinkers that justified Germany's belligerence—Einstein was one of the few German intellectuals to distance himself from it and sign the alternative, eirenic "Manifesto to the Europeans" instead. But this expression of his doubts about German policy did not prevent him from being elected to a two-year term as president of the German Physical Society in 1916. And when the Kaiser Wilhelm Institute for Physics opened its doors the following year—its foundation delayed because of the war—Einstein was appointed its first director, just as Planck and Nernst had promised.
 
                             <p>Einstein was elected a Foreign Member of the Royal Netherlands Academy of Arts and Sciences in 1920, and a Foreign Member of the Royal Society in 1921. In 1922, he was awarded the 1921 Nobel Prize in Physics "for his services to Theoretical Physics, and especially for his discovery of the law of the photoelectric effect". At this point some physicists still regarded the general theory of relativity sceptically, and the Nobel citation displayed a degree of doubt even about the work on photoelectricity that it acknowledged: it did not assent to Einstein's notion of the particulate nature of light, which only won over the entire scientific community when S. N. Bose derived the Planck spectrum in 1924. That same year, Einstein was elected an International Honorary Member of the American Academy of Arts and Sciences.Britain's closest equivalent of the Nobel award, the Royal Society's Copley Medal, was not hung around Einstein's neck until 1925. He was elected an International Member of the American Philosophical Society in 1930.</p>
@@ -61,12 +61,12 @@ function career() {
                 <br />
                 <section>
 
-                    <h2 className="h2ii">1919: Putting general relativity to the test</h2>
+                    <h2 className="career-subheading1">1919: Putting general relativity to the test</h2>
                     <div>
-                        <img src={img3} id="image3" className="col-md-3 float-md-start mb-1 me-md-3" alt="image" />
+                        <img src={career_img3} id="career-image3" className="col-md-3 float-md-start mb-1 me-md-3" alt="image" />
 
                     </div>
-                    <div className="text">
+                    <div className="career-text">
                         <p>
                             In 1907, Einstein reached a milestone on his long journey from his special theory of relativity to a new idea of gravitation with the formulation of his equivalence principle, which asserts that an observer in an infinitesimally small box falling freely in a gravitational field would be unable to find any evidence that the field exists. In 1911, he used the principle to estimate the amount by which a ray of light from a distant star would be bent by the gravitational pull of the Sun as it passed close to the Sun's photosphere (that is, the Sun's apparent surface). He reworked his calculation in 1913, having now found a way to model gravitation with the Riemann curvature tensor of a non-Euclidean four-dimensional spacetime. By the fall of 1915, his reimagining of the mathematics of gravitation in terms of Riemannian geometry was complete, and he applied his new theory not just to the behavior of the Sun as a gravitational lens but also to another astronomical phenomenon, the precession of the perihelion of Mercury (a slow drift in the point in Mercury's elliptical orbit at which it approaches the Sun most closely).A total eclipse of the Sun that took place on 29 May 1919 provided an opportunity to put his theory of gravitational lensing to the test, and observations performed by Sir Arthur Eddington yielded results that were consistent with his calculations. Eddington's work was reported at length in newspapers around the world. On 7 November 1919, for example, the leading British newspaper, The Times, printed a banner headline that read: "Revolution in Science – New Theory of the Universe – Newtonian Ideas Overthrown".
                         </p>
@@ -75,10 +75,10 @@ function career() {
                 </section>
                 <br />
                 <section>
-                    <h2 className="h2iii">1921–1923: Coming to terms with fame</h2>
+                    <h2 className="career-subheading2">1921–1923: Coming to terms with fame</h2>
                     <div className="clearfix">
-                        <img src={img4} id="image4" className="col-md-3 float-md-end mb-2 ms-md-3" alt="image" />
-                        <div className="text">
+                        <img src={career_img4} id="career-image4" className="col-md-3 float-md-end mb-2 ms-md-3" alt="image" />
+                        <div className="career-text">
                             <p>With Eddington's eclipse observations widely reported not just in academic journals but by the popular press as well, Einstein became "perhaps the world's first celebrity scientist", a genius who had shattered a paradigm that had been basic to physicists' understanding of the universe since the seventeenth century.</p>
                             <p>Einstein began his new life as an intellectual icon in America, where he arrived on 2 April 1921. He was welcomed to New York City by Mayor John Francis Hylan, and then spent three weeks giving lectures and attending receptions. He spoke several times at Columbia University and Princeton, and in Washington, he visited the White House with representatives of the National Academy of Sciences. He returned to Europe via London, where he was the guest of the philosopher and statesman Viscount Haldane. He used his time in the British capital to meet several people prominent in British scientific, political or intellectual life, and to deliver a lecture at King's College. In July 1921, he published an essay, "My First Impression of the U.S.A.", in which he sought to sketch the American character, much as had Alexis de Tocqueville in Democracy in America (1835). He wrote of his transatlantic hosts in highly approving terms: "What strikes a visitor is the joyous, positive attitude to life ... The American is friendly, self-confident, optimistic, and without envy."
                                 In 1922, Einstein's travels were to the old world rather than the new. He devoted six months to a tour of Asia that saw him speaking in Japan, Singapore and Sri Lanka (then known as Ceylon). After his first public lecture in Tokyo, he met Emperor Yoshihito and his wife at the Imperial Palace, with thousands of spectators thronging the streets in the hope of catching a glimpse of him. (In a letter to his sons, he wrote that Japanese people seemed to him to be generally modest, intelligent and considerate, and to have a true appreciation of art. But his picture of them in his diary was less flattering: "the intellectual needs of this nation seem to be weaker than their artistic ones – natural disposition?" His journal also contains views of China and India which were uncomplimentary. Of Chinese people, he wrote that "even the children are spiritless and look obtuse... It would be a pity if these Chinese supplant all other races. For the likes of us the mere thought is unspeakably dreary".) He was greeted with even greater enthusiasm on the last leg of his tour, in which he spent twelve days in Mandatory Palestine, newly entrusted to British rule by the League of Nations in the aftermath of the First World War. Sir Herbert Samuel, the British High Commissioner, welcomed him with a degree of ceremony normally only accorded to a visiting head of state, including a cannon salute. One reception held in his honor was stormed by people determined to hear him speak: he told them that he was happy that Jews were beginning to be recognized as a force in the world.</p>
@@ -92,24 +92,25 @@ function career() {
                 <br />
                 <section>
                     <div className="clearfix">
-                        <h2 className="h2iv">1922–1932: Serving the League of Nations</h2>
-                        <img src={img5} id="image5" className="col-md-4 float-md-start mb-1 me-md-3" alt="image" />
-                        <div className="text">
+                        <h2 className="career-subheading3">1922–1932: Serving the League of Nations</h2>
+                        <img src={career_img5} id="career-image5" className="col-md-4 float-md-start mb-1 me-md-3" alt="image" />
+                        <div className="career-text">
                             <p>From 1922 until 1932, with the exception of a few months in 1923 and 1924, Einstein was a member of the Geneva-based International Committee on Intellectual Cooperation of the League of Nations, a group set up by the League to encourage scientists, artists, scholars, teachers and other people engaged in the life of the mind to work more closely with their counterparts in other countries. He was appointed as a German delegate rather than as a representative of Switzerland because of the machinations of two Catholic activists, Oskar Halecki and Giuseppe Motta. By persuading Secretary General Eric Drummond to deny Einstein the place on the committee reserved for a Swiss thinker, they created an opening for Gonzague de Reynold, who used his League of Nations position as a platform from which to promote traditional Catholic doctrine. Einstein's former physics professor Hendrik Lorentz and the Polish chemist Marie Curie were also members of the committee. </p>
                         </div>
                     </div>
                 </section>
                 <br />
                 <section>
-                    <h1 className="Header2">Scientific career</h1><hr className="hr2" />
-                    <div className="text">
+                    <h1 className="career-Header">Scientific career</h1>
+                    <hr className="career-hr" />
+                    <div className="career-text">
                         <p>
                             Throughout his life, Einstein published hundreds of books and articles. He published more than 300 scientific papers and 150 non-scientific ones. On 5 December 2014, universities and archives announced the release of Einstein's papers, comprising more than 30,000 unique documents. Einstein's intellectual achievements and originality have made the word "Einstein" synonymous with "genius”. In addition to the work he did by himself he also collaborated with other scientists on additional projects including the Bose–Einstein statistics, the Einstein refrigerator and others.
                         </p>
                     </div>
 
-                    <h2 className="h2v">1905 – Annus Mirabilis papers</h2>
-                    <div className="text">
+                    <h2 className="career-subheading4">1905 – Annus Mirabilis papers</h2>
+                    <div className="career-text">
                         <p>The Annus Mirabilis papers are four articles pertaining to the photoelectric effect (which gave rise to quantum theory), Brownian motion, the special theory of relativity, and E = mc2 that Einstein published in the Annalen der Physik scientific journal in 1905. These four works contributed substantially to the foundation of modern physics and changed views on space, time, and matter. The four papers are:</p>
                     </div>
                     <div className="table-responsive">
--- src/components/life/life.jsx
+++ src/components/life/life.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import einstein from '../image/EINSTEIN.png';
+import life_einstein_png from '../image/EINSTEIN.png';
 import '../../styles/life.css';
 
 
@@ -9,19 +9,19 @@ function life() {
         <div>
             <div className="container">
 
-                <div className="subheading1">(Childhood,Youth and Education)</div>
-                <hr />
+                <div className="life-subheading">(Childhood,Youth and Education)</div>
+                <hr classname="life-hr" />
 
                 <br />
                 <div className="container">
                     <div class="clearfix">
-                        <img src={einstein} className="col-md-6 float-md-start mb-3 me-md-3  einstein_png" alt="..." />
-                        <section className="text">
+                        <img src={life_einstein_png} className="col-md-6 float-md-start mb-3 me-md-3  einstein_png" alt="..." />
+                        <section className="life-text">
                             Einstein was born on March 14, 1879, in Ulm, Germany, a town that today has a population of just more than 120,000.
                             His parents, secular Ashkenazi Jews, were Hermann Einstein(a salesman and engineer), and Pauline Koch.
                         </section>
 
-                        <section className="text">
+                        <section className="life-text">
                             <p />In 1880, the family moved to Munich's borough of Ludwigsvorstadt-Isarvorstadt, where Einstein's father and his uncle Jakob founded Elektrotechnische Fabrik J. Einstein & Cie,
                             a company that manufactured electrical equipment based on direct current.
                         </section>
